d5359d5cd0b0f69775cc6a8e46c06240f8c64c2f
Andres Felipe Losada <nedrocoli@gmail.com>
2025-02-22 20:43:20 -0500

    finalizado servicio de anonimizacion

--- services/seguridad/modulos/anonimizacion/aplicacion/__init__.py
+++ services/seguridad/modulos/anonimizacion/aplicacion/__init__.py
@@ -1,9 +1,7 @@
 from pydispatch import dispatcher
 from .handlers import HandlerAnonimizacionIntegracion
-from seguridad.modulos.anonimizacion.dominio.eventos import AnonimizacionCreada, AnonimizacionIniciada, AnonimizacionCancelada, AnonimizacionFinalizada
+from seguridad.modulos.anonimizacion.dominio.eventos import AnonimizacionIniciada, AnonimizacionCancelada, AnonimizacionFinalizada
 
-dispatcher.connect(HandlerAnonimizacionIntegracion.handle_anonimizacion_creada,
-                   signal=f'{AnonimizacionCreada.__name__}Integracion')
 dispatcher.connect(HandlerAnonimizacionIntegracion.handle_anonimizacion_iniciada,
                    signal=f'{AnonimizacionIniciada.__name__}Integracion')
 dispatcher.connect(HandlerAnonimizacionIntegracion.handle_anonimizacion_finalizada,
